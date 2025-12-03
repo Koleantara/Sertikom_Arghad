b3a69cc41d9e4a1fd208b15b4c7fbbddcac115ce
unknown <arghadv169@gmail.com>
2025-12-03 09:04:52 +0700

    fix6

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,28 +1,44 @@
 const navbar = document.querySelector(".navbar");
-
 window.addEventListener("scroll" ,() => {
     const posisi = window.scrollY > 0;
     navbar.classList.toggle("Scrolling-active", posisi);
 });
-const form = document.getElementById("myForm");
-const emailInput = document.getElementById("email");
-const emailError = document.getElementById("email_error");
 
-form.addEventListener("submit", function (event){
-        event.preventDefault();
-        let isValid = true;
+const form = document.getElementById('myform');
+        const nama = document.getElementById('nama');
+        const email = document.getElementById('email');
+        const subjek = document.getElementById('subjek');
 
-        emailError.textContent = "";
+        const nama_error = document.getElementById('nama_error');
+        const email_error = document.getElementById('email_error');
+        const subjek_error = document.getElementById('subjek_error');
+            form.addEventListener('submit', function(event) {
+                event.preventDefault();
+                let isValid = true;
 
-        const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-        if (emailInput.value.trim() === "") {
-          emailError.textContent = "Email tidak boleh kosong !";
-          isValid = false;
-        } else if (emailInput.value.match(emailPattern)) {
-          emailError.textContent = "Format email tidak valid!";
-          isValid = false;
-        }
+            nama_error.textContent = '';
+            email_error.textContent = '';
+            subjek_error.textContent = '';
 
-        if (isValid) {
+            if (nama.value.trim().length < 3) {
+                nama_error.textContent = 'tolong isi nama anda.';
+                isValid = false;
             }
-});
+
+            // Validate Email
+            const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+            if (email.value.match(emailPattern)) {
+                email_error.textContent = 'email anda salah format.';
+                isValid = false;
+            }
+
+            // Validate subjek
+            if (subjek.value.length < 6) {
+                subjek_error.textContent = 'subjek tolong diisi.';
+                isValid = false;
+            }
+
+            // If any validation fails, prevent form submission
+            if (!isValid) {
+            }
+        });
